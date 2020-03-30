aef07405ae7d7e2690aca74242b2de55b66a2824
GwG <gwg@GwGs-MacBook-Air.local>
2020-03-30 19:01:56 -0400

    javascript, simple styling

--- src/main/java/com/george/projectmanagement/model/Project.java
+++ src/main/java/com/george/projectmanagement/model/Project.java
@@ -70,5 +70,7 @@ public class Project {
         this.employees = employees;
     }
 
+
+
     // end
 }
--- src/main/java/com/george/projectmanagement/repository/EmployeeRepository.java
+++ src/main/java/com/george/projectmanagement/repository/EmployeeRepository.java
@@ -14,11 +14,11 @@ public interface EmployeeRepository extends CrudRepository<Employee, Long> {
     @Override
     public List<Employee> findAll();
 
-    @Query(nativeQuery = true, value= "SELECT emp.first_name as firstName,  emp.last_name as lastName, emp.email, COUNT(pro.employee_id) as projectCount " +
+    @Query(nativeQuery = true, value= "SELECT emp.first_name as firstName,  emp.last_name as lastName, emp.email as email, COUNT(pro.employee_id) as projectCount " +
             "FROM employee emp left join project_employee pro on pro.employee_id = emp.employee_id " +
-            "GROUP BY  emp.first_name , emp.last_name order by 3 desc;")
+            "GROUP BY  emp.first_name , emp.last_name , emp.email order by 3 desc")
     public List<Employeeproject> employeeprojects();
 
 
-
+    //
 }
